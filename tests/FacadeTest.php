7ff0c70a059b878dc1331e11e2f376f950530c78
hisorange <hisoranger@gmail.com>
2017-12-02 13:37:38 +0100

    Facade test 100% coverage

--- tests/FacadeTest.php
+++ tests/FacadeTest.php
@@ -2,19 +2,27 @@
 
 namespace hisorange\BrowserDetect\Test;
 
+use Browser;
+use hisorange\BrowserDetect\ParserInterface;
+
 /**
  * Class FacadeTest
- * @package hisorange\BrowserDetect\Test
- * @coversDefaultClass hisorange\BrowserDetect\Facade
+ * @package            hisorange\BrowserDetect\Test
  */
 class FacadeTest extends TestCase
 {
     /**
-     * @covers ::getFacadeAccessor()
+     * @covers \hisorange\BrowserDetect\Facade
      * @throws \PHPUnit_Framework_AssertionFailedError
+     * @throws \PHPUnit_Framework_Exception
      */
-    public function testAccessibility()
+    public function testResolve()
     {
         $this->assertTrue(class_exists('Browser'));
+
+        $expected = ParserInterface::class;
+        $actual   = Browser::getFacadeRoot();
+
+        $this->assertInstanceOf($expected, $actual);
     }
 }
